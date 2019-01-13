ca9f19fb82df5b8081ac0d2f1a871716da647aa5
Jihyok Kim <jkimab@connect.ust.hk>
2019-01-13 22:47:58 +0900

    Updated design for creating promise

--- app/src/main/java/com/simsimhan/promissu/network/AuthAPI.java
+++ app/src/main/java/com/simsimhan/promissu/network/AuthAPI.java
@@ -1,7 +1,10 @@
 package com.simsimhan.promissu.network;
 
+import com.simsimhan.promissu.network.model.Promise;
+
 import io.reactivex.Observable;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
@@ -12,4 +15,11 @@ public interface AuthAPI {
     })
     @POST("user/login")
     Observable<Login.Response> loginKakao(@Body Login.Request loginRequest);
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json"
+    })
+    @POST("appointment")
+    Observable<Promise.Request> createPromise(@Header("Authorization") String token, @Body Promise.Request promiseRequest);
 }
--- app/src/main/java/com/simsimhan/promissu/network/model/Promise.java
+++ app/src/main/java/com/simsimhan/promissu/network/model/Promise.java
@@ -20,4 +20,68 @@ public class Promise {
             return name;
         }
     }
+
+    //    {
+//            "title": "테스트 모임3",
+//            "explain": "우리 함께 모여서 놀아요",
+//            "date": "2018-01-11 13:00",
+//            "location_x": "37.499385",
+//            "location_y": "127.029204",
+//            "admin_id": "tester"
+//    }
+    public static class Request {
+        private String title;
+        private Date date;
+        private float location_x;
+        private float location_y;
+        private Date delayTime;
+
+        public Request(String title, Date date, float location_x, float location_y, Date delayTime) {
+            this.title = title;
+            this.date = date;
+            this.location_x = location_x;
+            this.location_y = location_y;
+            this.delayTime = delayTime;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public float getLocation_x() {
+            return location_x;
+        }
+
+        public void setLocation_x(float location_x) {
+            this.location_x = location_x;
+        }
+
+        public float getLocation_y() {
+            return location_y;
+        }
+
+        public void setLocation_y(float location_y) {
+            this.location_y = location_y;
+        }
+
+        public Date getDelayTime() {
+            return delayTime;
+        }
+
+        public void setDelayTime(Date delayTime) {
+            this.delayTime = delayTime;
+        }
+    }
 }
