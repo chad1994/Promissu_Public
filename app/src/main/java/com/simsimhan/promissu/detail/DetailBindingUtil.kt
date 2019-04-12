2797e52343c693974828b8dad9b78eee95dfd05d
최준영 <chad1994@skuniv.ac.kr>
2019-04-12 21:55:23 +0900

    add setBottomSheet BindingAdapter

--- app/src/main/java/com/simsimhan/promissu/detail/DetailBindingUtil.kt
+++ app/src/main/java/com/simsimhan/promissu/detail/DetailBindingUtil.kt
@@ -1,7 +1,9 @@
 package com.simsimhan.promissu.detail
 
+import android.widget.ImageButton
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.simsimhan.promissu.R
 import com.simsimhan.promissu.detail.adapter.DetailUserStatusAdapter
 import com.simsimhan.promissu.network.model.Participant
 
@@ -11,5 +13,14 @@ fun setRvItems(recyclerView: RecyclerView, itemList: List<Participant.Response>?
         (recyclerView.adapter as DetailUserStatusAdapter).setData(itemList)
     } else {
         // TODO 데이터 정보 없음 처리.
+    }
+}
+
+@BindingAdapter("setBottomSheetIcon")
+fun setBottomSheetIcon(imageButton: ImageButton, isSpread:Boolean){
+    if(isSpread){
+        imageButton.setImageResource(R.drawable.ic_arrow_down)
+    }else{
+        imageButton.setImageResource(R.drawable.ic_arrow_up)
     }
 }
