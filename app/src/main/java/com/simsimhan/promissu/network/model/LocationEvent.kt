be02fd628f56926207c5740a5d1770ad730443dc
최준영 <chad1994@skuniv.ac.kr>
2019-04-13 18:46:52 +0900

    add, modify model LocationEvent, Participant

--- app/src/main/java/com/simsimhan/promissu/network/model/LocationEvent.kt
+++ app/src/main/java/com/simsimhan/promissu/network/model/LocationEvent.kt
@@ -1,2 +1,14 @@
 package com.simsimhan.promissu.network.model
 
+import java.util.*
+
+data class LocationEvent(
+        val apptId : Int,
+        val id: Int,
+        val lat: Double,
+        val lon: Double,
+        val partId: Int,
+        val point: Int,
+        val status: Int,
+        val timestamp: Date
+)
--- app/src/main/java/com/simsimhan/promissu/network/model/Participant.java
+++ app/src/main/java/com/simsimhan/promissu/network/model/Participant.java
@@ -4,10 +4,12 @@ public class Participant {
     public static class Response {
         private final int kakao_id;
         private final String nickname;
+        private final int participation;
 
-        public Response(int kakao_id, String nickname) {
+        public Response(int kakao_id, String nickname, int participation) {
             this.kakao_id = kakao_id;
             this.nickname = nickname;
+            this.participation = participation;
         }
 
         public int getKakao_id() {
@@ -16,6 +18,10 @@ public class Participant {
 
         public String getNickname() {
             return nickname;
+        }
+
+        public int getParticipation() {
+            return participation;
         }
     }
 }
