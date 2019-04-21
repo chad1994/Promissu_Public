97870d268b9b4064b4e00b005fb40ddc69765b6e
최준영 <chad1994@skuniv.ac.kr>
2019-04-21 20:51:00 +0900

    modify details

--- app/src/main/java/com/simsimhan/promissu/detail/DetailBindingUtil.kt
+++ app/src/main/java/com/simsimhan/promissu/detail/DetailBindingUtil.kt
@@ -3,6 +3,7 @@ package com.simsimhan.promissu.detail
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.simsimhan.promissu.R
@@ -18,6 +19,15 @@ fun setRvItems(recyclerView: RecyclerView, itemList: List<Participant.Response>?
     }
 }
 
+@BindingAdapter("detailEmptyTextVisible")
+fun detailEmptyTextVisible(text: TextView, list: List<Participant.Response>) {
+    if (list.isNullOrEmpty()) {
+        text.visibility = View.VISIBLE
+    } else {
+        text.visibility = View.GONE
+    }
+}
+
 @BindingAdapter("setBottomSheetIcon")
 fun setBottomSheetIcon(imageButton: ImageButton, isSpread: Boolean) {
     if (isSpread) {
@@ -30,24 +40,52 @@ fun setBottomSheetIcon(imageButton: ImageButton, isSpread: Boolean) {
 @BindingAdapter("setUserItemStatus")
 fun setUserItemStatus(imageview: ImageView, status: Int) {
     when (status) {
-        0 -> imageview.visibility = View.GONE
+        0 -> {
+                imageview.visibility = View.GONE
+        }
         1 -> {
-            imageview.visibility = View.VISIBLE
-            imageview.setImageResource(R.drawable.ic_icon_status_ing)
+                imageview.visibility = View.VISIBLE
+                imageview.setImageResource(R.drawable.ic_icon_status_ing)
         }
         2 -> {
-            imageview.visibility = View.VISIBLE
-            imageview.setImageResource(R.drawable.ic_icon_status_x)
+                imageview.visibility = View.VISIBLE
+                imageview.setImageResource(R.drawable.ic_icon_status_x)
         }
         3 -> {
-            imageview.visibility = View.VISIBLE
-            imageview.setImageResource(R.drawable.ic_icon_status_check)
+                imageview.visibility = View.VISIBLE
+                imageview.setImageResource(R.drawable.ic_icon_status_check)
         }
         4 -> {
-            imageview.visibility = View.GONE
+                imageview.visibility = View.VISIBLE
+                imageview.setImageResource(R.drawable.ic_icon_attend_complete)
         }
         else -> {
-            imageview.visibility = View.GONE
+                imageview.visibility = View.GONE
+        }
+    }
+}
+
+@BindingAdapter("setUserItemAttended")
+fun setUserItemAttended(text: TextView, status: Int) {
+    when (status) {
+        4-> text.visibility = View.VISIBLE
+        else -> text.visibility = View.GONE
+    }
+}
+
+@BindingAdapter("visibilityByArrive", "visibilityByArrive2", "visibilityByArrive3")
+fun visibilityByArrive(text: TextView, visibility: Boolean, attended: Boolean, isSocketOpen: Boolean) {
+    if (text.id == R.id.detail_attend_button) {
+        if (visibility && !attended && isSocketOpen) {
+            text.visibility = View.VISIBLE
+        } else {
+            text.visibility = View.GONE
+        }
+    } else {
+        if ((visibility && !attended) || !isSocketOpen) {
+            text.visibility = View.GONE
+        } else {
+            text.visibility = View.VISIBLE
         }
     }
 }
