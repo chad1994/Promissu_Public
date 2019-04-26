ea6fbc7510cea2c1f7fbe58f0ed3861900594146
최준영 <chad1994@skuniv.ac.kr>
2019-04-26 17:13:28 +0900

    add binding adapter on promise activity

--- app/src/main/java/com/simsimhan/promissu/detail/PromiseDetailActivity.kt
+++ app/src/main/java/com/simsimhan/promissu/detail/PromiseDetailActivity.kt
@@ -14,7 +14,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -34,7 +33,6 @@ import com.simsimhan.promissu.R
 import com.simsimhan.promissu.databinding.ActivityDetailPromiseBinding
 import com.simsimhan.promissu.detail.adapter.DetailUserStatusAdapter
 import com.simsimhan.promissu.network.model.Promise
-import timber.log.Timber
 
 
 class PromiseDetailActivity : AppCompatActivity(), OnMapReadyCallback {
--- app/src/main/java/com/simsimhan/promissu/promise/PromiseBindingUtil.kt
+++ app/src/main/java/com/simsimhan/promissu/promise/PromiseBindingUtil.kt
@@ -1,2 +1,81 @@
 package com.simsimhan.promissu.promise
 
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.simsimhan.promissu.R
+import com.simsimhan.promissu.network.model.Promise
+import org.joda.time.DateTime
+import org.joda.time.Days
+import org.joda.time.Hours
+import org.joda.time.Minutes
+
+@BindingAdapter("setItemLeftTime1", "setItemLeftTime2")
+fun setItemLeftTime(text: TextView, isPast: Boolean, response: Promise.Response) {
+    if (isPast) {
+        text.visibility = View.GONE
+    } else {
+        text.visibility = View.VISIBLE
+        val now = DateTime()
+        val promiseStartDate = DateTime(response.start_datetime)
+        val daysDifference = Days.daysBetween(now, promiseStartDate)
+
+        if (daysDifference.days == 0) {
+            val hoursDifference = Hours.hoursBetween(now, promiseStartDate)
+            val minuteDifference = Minutes.minutesBetween(now, promiseStartDate)
+            if (hoursDifference.hours <= 1) {
+                if (minuteDifference.minutes <= 60) {
+                    text.text = ""
+                } else {
+                    text.text = minuteDifference.minutes.toString()
+                }
+            } else {
+                text.text = hoursDifference.hours.toString()
+            }
+        } else {
+            text.text = daysDifference.days.toString()
+        }
+    }
+}
+
+@BindingAdapter("setItemLeftLabel1", "setItemLeftLabel2")
+fun setItemLeftLabel(text: TextView, isPast: Boolean, response: Promise.Response) {
+    if (isPast) {
+        text.text = "" + (response.start_datetime.month + 1) + "." + (response.start_datetime.date)
+    } else {
+        val now = DateTime()
+        val promiseStartDate = DateTime(response.start_datetime)
+        val daysDifference = Days.daysBetween(now, promiseStartDate)
+
+        if (daysDifference.days == 0) {
+            val hoursDifference = Hours.hoursBetween(now, promiseStartDate)
+            val minuteDifference = Minutes.minutesBetween(now, promiseStartDate)
+            if (hoursDifference.hours <= 1) {
+                if (minuteDifference.minutes <= 60) {
+                    text.text = "대기중"
+                    text.setTextColor(ContextCompat.getColor(text.context, R.color.sub_color))
+                } else {
+                    text.text = "분 남음"
+                    text.setTextColor(ContextCompat.getColor(text.context, R.color.sub_color))
+                }
+            } else {
+                text.text = "시간 남음"
+                text.setTextColor(ContextCompat.getColor(text.context, R.color.sub_color))
+            }
+        } else {
+            text.text = "일 남음"
+            text.setTextColor(ContextCompat.getColor(text.context, R.color.black))
+        }
+    }
+}
+
+@BindingAdapter("setContainerBg")
+fun setContainerBg(container:ConstraintLayout,isPast: Boolean){
+    if(isPast){
+        container.setBackgroundColor(ContextCompat.getColor(container.context, R.color.past_background_color))
+    }else{
+        container.setBackgroundColor(ContextCompat.getColor(container.context, R.color.background_grey))
+    }
+}
