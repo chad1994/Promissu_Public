46a224a88deaeee64006b12d6a5fc31254409f9b
최준영 <chad1994@skuniv.ac.kr>
2019-06-01 21:08:13 +0900

    add Appointment Model

--- app/src/main/java/com/simsimhan/promissu/network/model/Appointment.kt
+++ app/src/main/java/com/simsimhan/promissu/network/model/Appointment.kt
@@ -1,2 +1,12 @@
 package com.simsimhan.promissu.network.model
 
+import android.os.Parcel
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Appointment(
+        @SerializedName("appointment")val promise: Promise.Response,
+        val status : Int
+) : Parcelable
