3d08549c578701a679c2e2d91d93d32ce6e9173d
최준영 <chad1994@skuniv.ac.kr>
2019-06-04 22:04:50 +0900

    add detail Past activity,viewModel

--- app/src/main/java/com/simsimhan/promissu/pastdetail/DetailPastActivity.kt
+++ app/src/main/java/com/simsimhan/promissu/pastdetail/DetailPastActivity.kt
@@ -1,2 +1,7 @@
 package com.simsimhan.promissu.pastdetail
 
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailPastActivity : AppCompatActivity(){
+
+}
--- app/src/main/java/com/simsimhan/promissu/pastdetail/DetailPastViewModel.kt
+++ app/src/main/java/com/simsimhan/promissu/pastdetail/DetailPastViewModel.kt
@@ -1,2 +1,12 @@
 package com.simsimhan.promissu.pastdetail
 
+import androidx.lifecycle.ViewModel
+import com.simsimhan.promissu.BaseViewModel
+import com.simsimhan.promissu.network.model.Promise
+
+class DetailPastViewModel(val promise: Promise.Response) : BaseViewModel(){
+    init {
+
+    }
+
+}
