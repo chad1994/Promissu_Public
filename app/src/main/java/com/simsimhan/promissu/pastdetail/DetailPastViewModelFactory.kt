8cd46d35ab7844a9e6f81c46f65339a3c79f79d4
최준영 <chad1994@skuniv.ac.kr>
2019-06-05 18:34:54 +0900

    add detailPastViewModel

--- app/src/main/java/com/simsimhan/promissu/pastdetail/DetailPastViewModelFactory.kt
+++ app/src/main/java/com/simsimhan/promissu/pastdetail/DetailPastViewModelFactory.kt
@@ -1,2 +1,12 @@
 package com.simsimhan.promissu.pastdetail
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.simsimhan.promissu.network.model.Appointment
+import com.simsimhan.promissu.network.model.Promise
+
+class DetailPastViewModelFactory(val promise: Appointment) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(Appointment::class.java).newInstance(promise)
+    }
+}
