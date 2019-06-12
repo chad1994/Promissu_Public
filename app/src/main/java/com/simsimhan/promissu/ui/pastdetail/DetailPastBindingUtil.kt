c33e12864c73ca71a3c04c747bf8a6f5a7fcf705
최준영 <chad1994@skuniv.ac.kr>
2019-06-12 20:44:40 +0900

    setting data to items

--- app/src/main/java/com/simsimhan/promissu/ui/pastdetail/DetailPastBindingUtil.kt
+++ app/src/main/java/com/simsimhan/promissu/ui/pastdetail/DetailPastBindingUtil.kt
@@ -2,6 +2,10 @@ package com.simsimhan.promissu.ui.pastdetail
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.simsimhan.promissu.network.model.Participant
+import com.simsimhan.promissu.ui.detail.adapter.DetailUserStatusAdapter
+import com.simsimhan.promissu.ui.pastdetail.adapter.DetailPastAdapter
 
 @BindingAdapter("setAttendanceText")
 fun setAttendanceText(view: TextView,status: Int){
@@ -15,5 +19,15 @@ fun setAttendanceText(view: TextView,status: Int){
         4->{
             view.text = "결석했습니다"
         }
+    }
+}
+
+
+@BindingAdapter("setDetailPastItems")
+fun setDetailPastItems(recyclerView: RecyclerView, itemList: List<Participant.Response>?) {
+    if (itemList != null) {
+        (recyclerView.adapter as DetailPastAdapter).setData(itemList)
+    } else {
+        // TODO 데이터 정보 없음 처리.
     }
 }
