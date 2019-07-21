22038b62a7eedbc5ec71a7a174482dd07645f470
최준영 <chad1994@skuniv.ac.kr>
2019-07-21 23:30:49 +0900

    create numberPickerFragment

--- app/src/main/java/com/simsimhan/promissu/ui/promise/create/NumberPickerFragment.kt
+++ app/src/main/java/com/simsimhan/promissu/ui/promise/create/NumberPickerFragment.kt
@@ -1,2 +1,77 @@
 package com.simsimhan.promissu.ui.promise.create
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.NumberPicker
+import androidx.fragment.app.DialogFragment
+import com.simsimhan.promissu.R
+
+
+class NumberPickerFragment : DialogFragment() {
+    private lateinit var valueChangeListener: NumberPicker.OnValueChangeListener
+    lateinit var title: String   //dialog 제목
+    lateinit var subtitle: String    //dialog 부제목
+    var minvalue: Int = 0   //입력가능 최소값
+    var maxvalue: Int = 0  //입력가능 최대값
+    var step: Int = 0   //선택가능 값들의 간격
+    var defvalue: Int = 0 //dialog 시작 숫자 (현재값)
+    var unit : String = " "
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val numberPicker = NumberPicker(activity)
+
+        title = arguments!!.getString("title")
+        subtitle = arguments!!.getString("subtitle")
+        minvalue = arguments!!.getInt("minvalue")
+        maxvalue = arguments!!.getInt("maxvalue")
+        step = arguments!!.getInt("step")
+        defvalue = arguments!!.getInt("defValue")
+        unit = arguments!!.getString("unit")
+
+        val myValues = getArrayWithSteps(minvalue, maxvalue, step, unit)
+
+        numberPicker.minValue = 0
+        numberPicker.maxValue = (maxvalue - minvalue) / step
+        numberPicker.displayedValues = myValues
+        numberPicker.value = (defvalue - minvalue) / step
+        numberPicker.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+        numberPicker.dividerDrawable
+
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle(title)
+        builder.setMessage(subtitle)
+        builder.setPositiveButton("OK") { _, _ ->
+            valueChangeListener.onValueChange(numberPicker,
+                    numberPicker.value, numberPicker.value)
+            dismiss()
+        }
+        builder.setNegativeButton("CANCEL") { _, _ ->
+            dismiss()
+        }
+        builder.setView(numberPicker)
+
+        return builder.create()
+    }
+
+    fun getValueChangeListener(): NumberPicker.OnValueChangeListener {
+        return valueChangeListener
+    }
+
+    fun setValueChangeListener(valueChangeListener: NumberPicker.OnValueChangeListener) {
+        this.valueChangeListener = valueChangeListener
+    }
+
+
+    private fun getArrayWithSteps(min: Int, max: Int, step: Int,unit:String): Array<String?> {
+        val number_of_array = (max - min) / step + 1
+
+        val result = arrayOfNulls<String>(number_of_array)
+
+        for (i in 0 until number_of_array) {
+            result[i] = (min + step * i).toString() + unit
+        }
+        return result
+    }
+
+}
